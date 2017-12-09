c7b54c7d3db5d0777209f1ac821663ac3fa96062
Tyler Barnett <tyler.barnett7190@gmail.com>
2017-12-09 09:30:44 -0600

    map now showing on competition detail page. need to be able to center it on each individual competition now

--- scramble-app/app/controllers/single-competition-controller.js
+++ scramble-app/app/controllers/single-competition-controller.js
@@ -2,11 +2,24 @@
 
 app.controller('singleCompetitionController', function($scope, competitionFactory, golfCourseFactory, $routeParams) {
 
+
+
+	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	    maxZoom: 18,
+	    id: 'mapbox.streets',
+	    accessToken: 'pk.eyJ1IjoidHlsZXJiMyIsImEiOiJjamF6Z3gwM3oxaHQ4MzJvOXhsZ3g3bDFlIn0.Jo7_JkWf41s181_JG0MCxQ'
+	}).addTo(mymap);
+
+
 const showCompetition = function() {
     competitionFactory.getSingleCompetition($routeParams.itemId).then(data => {
       $scope.competition = data;
     });
 };
+
 
 showCompetition();
 
